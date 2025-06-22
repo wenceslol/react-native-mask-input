009f969b52cdfd73ffe9e8312ab9dade7aa609ee
Wenceslau <thiago.sw35@gmail.com>
2025-06-22 08:36:51 -0300

    Lógica do Sufixo
    Testando lógicas para o sufixo não atrapalhar inputs apagando os valores.

--- src/createNumberMask.ts
+++ src/createNumberMask.ts
@@ -4,10 +4,18 @@ import type { Mask } from './formatWithMask.types';
 import type { CreateNumberMaskProps } from './createNumberMask.types';
 
 export default function createNumberMask(props?: CreateNumberMaskProps): Mask {
-  const { delimiter = '.', precision = 1, prefix = [], suffix = [], separator = ',' } = props || {};
+  const { delimiter = '.', precision = 0, prefix = [], suffix = [], separator = ',' } = props || {};
 
   return (value?: string) => {
-    const numericValue = value?.replace(/\D+/g, '') || '';
+
+    let numericValue = value?.replace(/\D+/g, '') || '';
+    const newValue = value?.replace(/\D+/g, '') || '';
+    
+    const testeValue = value?.replace(/[^0-9]/g, '');
+    if (testeValue?.length === 1){
+      let numericValue = newValue.slice(0, -1);
+    }
+    
 
     let mask: MaskArray = numericValue.split('').map(() => /\d/);
 
